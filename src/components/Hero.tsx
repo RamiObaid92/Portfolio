631c96429af8bcd567211ab01fc654d4ffd153f9
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-06-12 14:40:29 +0200

    closing tag quick fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -66,6 +66,11 @@ const Hero = () => {
               </div>
               <div className="absolute bottom-0 right-10 bg-white p-2 rounded-full shadow-lg">
                 <span className="text-yellow-600 font-bold text-xl">
+                  JavaScript
+                </span>
+              </div>
+              <div className="absolute top-1/2 -right-10 bg-white p-2 rounded-full shadow-lg">
+                <span className="text-blue-500 font-bold text-xl">
                   TypeScript
                 </span>
               </div>
