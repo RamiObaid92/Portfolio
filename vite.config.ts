f263c2c109b645c201acac0480f4d6e9bb1c43b7
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-07-21 16:35:27 +0200

    fix: Update base path in Vite configuration for correct deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   css: {
     devSourcemap: true,
